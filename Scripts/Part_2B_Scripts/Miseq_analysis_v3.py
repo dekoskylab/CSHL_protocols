f83ecd0179ed718fc135a76c97d4bb234a30861c
Immune Engineering Laboratory <79920645+dekoskylab@users.noreply.github.com>
2023-10-23 18:21:02 -0400

    Update and rename Miseq_analysis_v2.py to Miseq_analysis_v3.py

---
+++ Scripts/Part_2B_Scripts/Miseq_analysis_v3.py
@@ -0,0 +1,65 @@
+import os
+import sys
+import re
+import subprocess
+f_name=sys.argv[1]
+len_gene=sys.argv[2]
+template=sys.argv[3]
+mm=re.search('_L001',f_name).start()
+print (mm)
+expt_name=f_name[0:mm]
+print (expt_name)
+VDJaa_fname=expt_name+'_VDJaa.txt'
+print (VDJaa_fname)
+cmd1='wc -l '+f_name
+output=os.system(cmd1)
+
+
+#Print amino acid sequences from IgBlast output files
+
+#cmd2='grep -v sequence '+f_name+ "|awk '"+'{if(length($48)>5) print  $15 }'+"' >"+VDJaa_fname+"_ff"
+#os.system(cmd2)
+
+cmd2='bash tab_to_fasta.sh ' + f_name + " " +  VDJaa_fname
+print(cmd2)
+os.system(cmd2)
+#cmd2_1='cat '+f_name+ "|awk '"+'{if(length($4)>5) print  $12 }'+"' >"+VDJaa_fname+"_addn"
+#os.system(cmd2_1)
+#cmd2_2='cat '+ VDJaa_fname+" "+ VDJaa_fname+"_addn "+" > "+VDJaa_fname+"_ff"
+#print (cmd2_2)
+#os.system(cmd2_2)
+
+cmd_tr = 'perl translate.pl -f 1 ' + VDJaa_fname + "_nt > " + VDJaa_fname + "_tr"
+cmd_cl = "fasta_formatter -i " + VDJaa_fname + "_tr | grep -v '^>' | sed 's/.*VLA//g' | sed 's/ASTG.*//g' > " + VDJaa_fname + "_ff"
+
+os.system(cmd_tr)
+os.system(cmd_cl)
+
+
+
+#Split files into smaller chunks
+cmd3='split -l 100000 '+VDJaa_fname+"_ff"+" "+VDJaa_fname+"_ff"+"_"
+os.system(cmd3)
+cmd4='ls | grep '+VDJaa_fname+"_ff"+'_'
+file_count=os.popen(cmd4).read()
+files=[]
+files=file_count.split()
+print (files)
+
+for i in range (len(files)):
+        command="python jobsubmit.py "+files[i]+" "+len_gene+ " "+template
+        cmd5="echo '"+command+"' >temp_command"
+        print (cmd5)
+        os.system(cmd5)
+        cmd6="cat blank_jobsubmit temp_command"+">"+"temp_submit"
+        os.system(cmd6)
+        cmd7="sbatch temp_submit"
+        os.system(cmd7)
+
+cmd="python sorting.py "+f_name+" "+len_gene+ " " + template
+cmd8="echo '"+cmd+"' >temp"
+os.system(cmd8)
+print (cmd8)
+cmd9="cat blank_jobsubmit temp "+"> "+VDJaa_fname+"_ff-"+"sort_blast"
+print (cmd9)
+os.system(cmd9)
