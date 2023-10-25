df9339ea594b0fe785a41098bb0548b4844523a7
Immune Engineering Laboratory <79920645+dekoskylab@users.noreply.github.com>
2023-10-25 13:29:32 -0400

    Add files via upload

---
+++ Scripts/Scripts for protocol 4/CDR3motif_search_part1_v2.1.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#
+#Process two FASTA files R1 and R2 for VH:VL pairs using CDR3 motif
+#Input:   CDR3motif_search.sh R1_fasta_file R2_fasta_file barcodes_file
+#barcodes_files :  mouse_barcodes.txt  human_barcodes.txt  rhesus_barcodes.txt
+
+R1fasta=$1
+R2fasta=$2
+barcodes=$3
+
+date; perl vpairhumanalysis-igblast-isotype_BDmod.pl "$R1fasta" "$R2fasta" "$barcodes"; date
+
--- Scripts/Scripts for protocol 4/check_hold-mod_sbatch.sh
+++ Scripts/Scripts for protocol 4/check_hold-mod_sbatch.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
+#SBATCH --partition=sixhour
 #SBATCH --ntasks-per-node=2
-#SBATCH --nodes=1
+#SBATCH --nodes=2
+#SBATCH --constraint=ib
 #SBATCH --mem-per-cpu=40gb
+#SBATCH --time=0-06:00:00
 #SBATCH --output=mpi_%j.log
 #SBATCH --mail-type=FAIL
-#SBATCH --mail-user=afahad@mgh.harvard.edu
-
+#SBATCH --mail-user=ahmed.fahad@ku.edu
 
 while read line
 do
@@ -13,15 +15,15 @@ do
         file1=$(echo "$line" | awk '{print $1}')
         file2=$(echo "$line" | awk '{print $2}')
         prefix=$(echo "$line" | awk '{print $3}')
-        barcodes=$(echo "$line" | awk '{print $4}')
-        #barcodes=$(echo "human_barcodes.txt")
+        #barcodes=$(echo "$line" | awk '{print $4}')
+        barcodes=$(echo "human_barcodes.txt")
         echo $file1
         echo $file2
         echo $barcodes
 
         #Send command for quality filtering, calling qualityfilter_script_human_v1.0.sh.  SPECIFY THE BARCODES REQUIRED USING THE INPUT BARCODES VARIABLE AT THE TOP
         rm $prefix"_queuelist.txt"
-        cat large_jobsubmit | awk -v file1=$file1 -v file2=$file2 -v prefix=$prefix -v barcodes=$barcodes '{print $0}; END {print "bash qualityfilter_script_v2.0.sh " file1 " " file2 " " prefix " " barcodes}' > $prefix"QFjobsubmit"
+        cat large_jobsubmit | awk -v file1=$file1 -v file2=$file2 -v prefix=$prefix -v barcodes=$barcodes '{print $0}; END {print "bash qualityfilter_script_human_v1.0.sh " file1 " " file2 " " prefix " " barcodes}' > $prefix"QFjobsubmit"
         sbatch $prefix"QFjobsubmit" | awk '{print $4 ","}' >> $prefix"_queuelist.txt"
         echo "Command ran successfully"
         cat $prefix"_queuelist.txt" | tr -d '\n' | tr -d '\r' | sed 's/^,/:/' | sed 's/,/:/g;s/:$//' > $prefix"_queuelist2.txt"
@@ -40,5 +42,5 @@ do
 
         sleep 3
 
-done < pairlist.txt
+done < pairlist.txt 
 
