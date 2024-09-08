c7926f8a5fdabf54889885f5f5e78a43e4c5cf83
Immune Engineering Laboratory <79920645+dekoskylab@users.noreply.github.com>
2024-09-08 08:27:39 -0400

    Updated scripts

--- Scripts/Scripts for Clonal Lineage and Gene Diversity Analysis of Paired Antibody Heavy and Light Chains/qualityfilter_script_human_v2.0.sh
+++ Scripts/Scripts for Clonal Lineage and Gene Diversity Analysis of Paired Antibody Heavy and Light Chains/qualityfilter_script_human_v2.0.sh
@@ -1,5 +1,4 @@
-
-	file1=$1
+file1=$1
 	file2=$2
 	prefix=$3
 	barcodes=$4
@@ -23,8 +22,8 @@
 	join -j 1 -o 1.1 1.2 1.3 $file1"_q20p50.fasta_temp" $file2"_q20p50.fasta_temp" | awk '{print $1 " " $2 "\n" $3}' > $prefix"_r1_q20p50_filtered.fasta"
 	join -j 1 -o 2.1 2.2 2.3 $file1"_q20p50.fasta_temp" $file2"_q20p50.fasta_temp" | awk '{print $1 " " $2 "\n" $3}' > $prefix"_r2_q20p50_filtered.fasta"
 	echo "$f joined into filtered files"
-	split -l 100000 $prefix"_r1_q20p50_filtered.fasta" $prefix"_r1_q20p50_filtered.fasta_"
-	split -l 100000 $prefix"_r2_q20p50_filtered.fasta" $prefix"_r2_q20p50_filtered.fasta_"
+	split -l 200000 $prefix"_r1_q20p50_filtered.fasta" $prefix"_r1_q20p50_filtered.fasta_"
+	split -l 200000 $prefix"_r2_q20p50_filtered.fasta" $prefix"_r2_q20p50_filtered.fasta_"
 	
 		
-	ls | grep $prefix"_r2_q20p50_filtered.fasta_" | grep -v temp | grep -v reads | awk '{print $1 " " $1}' | sed 's/_r2_q20p50/_r1_q20p50/' | awk -v barcodes=$barcodes '{print "bash CDR3motif_search_part1_v2.1.sh " $0 " " barcodes}' > $prefix"_commands_part1"
+	ls | grep $prefix"_r2_q20p50_filtered.fasta_" | grep -v temp | grep -v reads | awk '{print $1 " " $1}' | sed 's/_r2_q20p50/_r1_q20p50/' | awk -v barcodes=$barcodes '{print "CDR3motif_search_part1_v2.1.sh " $0 " " barcodes}' > $prefix"_commands_part1"
